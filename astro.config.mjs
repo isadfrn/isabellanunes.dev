0b2f604201f5e445e3241086ec104464efc30c63
Isabella Nunes <isadfrn@MacBook-Pro-de-Isabella.local>
2026-03-14 13:44:53 -0300

    refactor: Remo admin page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,9 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
   integrations: [react(), mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
