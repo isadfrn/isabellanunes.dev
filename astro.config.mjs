78fc87559a80ab229d6dd7bacb33740a3d9d4726
isadfrn <isadfrn@gmail.com>
2026-06-01 18:08:21 -0300

    feat: add base project

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  integrations: [react(), mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  i18n: {
+    defaultLocale: "pt",
+    locales: ["pt", "en"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+  site: "https://isabellanunes.dev",
+});
